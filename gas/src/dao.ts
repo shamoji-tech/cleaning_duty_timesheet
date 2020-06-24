71a7587a4f66fac1031c430752a73f6e33464958
shamoji-tech <suguru_toyohara@outlook.jp>
2020-06-24 21:35:04 +0900

    writeNameとchangeNextDutyを追加

--- gas/src/dao.ts
+++ gas/src/dao.ts
@@ -1,16 +1,19 @@
 const mainSheetName: string = "main"
 class SheetHandler {
-    
+    /* SheetHandler
+     * 
+     */
     mainSheet : GoogleAppsScript.Spreadsheet.Sheet;
     lastRow : number ;
     sheetData: any[][];
     personsDataArray: PersonSheet[]= new Array;
     constructor(){
         this.mainSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(mainSheetName);
-        this.lastRow = this.mainSheet.getLastRow()-1;
-        this.sheetData = this.mainSheet.getRange(2,1,this.lastRow,5).getValues();
+        this.lastRow = this.mainSheet.getLastRow();
+        //TODO:マジックナンバー
+        this.sheetData = this.mainSheet.getRange(2,1,this.lastRow-1,5).getValues();
         this.sheetData.forEach(element =>{
-            this.personsDataArray.push(new PersonSheet(element));
+            this.personsDataArray.push(PersonSheet.fromSheetElement(element));
         });
     }
     
@@ -21,31 +24,104 @@ class SheetHandler {
             }
         }
         throw new Error("当番を見つけられませんでした。データが全て空か、当番項目が全てハイフンになっている可能性があります。");
-
+    }
+    writeNewPerson(person:Person){
+        const writeObject=PersonSheet.fromPerson(person)
+        //TODO:マジックナンバー
+        this.mainSheet.getRange(this.lastRow+1,1,1,5).setValues([writeObject.toValues()])
+    }
+    checkRoot(person:Person): boolean{
+        for (const item of this.personsDataArray){
+            if(person.slackId === item.id && item.root){
+                return true
+            }
+        }
+        return false
+    }
+    private changeDuty(id:number,isduty:boolean){
+        const valueToSet = ["-","*"][Number(isduty)]
+        //TODO: +2って言うマジックナンバーをどうにかする
+        this.mainSheet.getRange(id+2,1).setValue(valueToSet)
+    }
+    changeNextDuty(){
+        var changeFlag =false
+        for(var index=0; index<this.personsDataArray.length;index++){
+            if(changeFlag){
+                changeFlag=false
+                this.changeDuty(index,true)
+            }
+            if(this.personsDataArray[index].duty){
+                changeFlag=true
+                this.changeDuty(index,false)
+            }
+        }
+        if(changeFlag){
+            this.changeDuty(0,true)
+        }
     }
 }
+
+
+
 class PersonSheet {
     duty: boolean;
     root: boolean;
     name: string;
     id  : string;
     time: string;
-    constructor(element:any[]){
-        this.duty = element[0]==="*";
-        this.root = element[1]==="*";
-        this.name = element[2];
-        this.id   = element[3];
-        this.time = element[4];
+    constructor(duty:boolean,root:boolean,name:string,id:string,time:string){
+        this.duty = duty
+        this.root = root
+        this.name = name
+        this.id   = id
+        this.time = time
     }
     toPerson():Person{
         return new Person(this.name,this.id);
     }
+    toValues():any[]{
+        const duty = ["-","*"][Number(this.duty)]
+        const root = ["-","*"][Number(this.duty)]
+        return [duty,root,this.name,this.id,this.time]
+    }
+    static fromSheet(elements:any[][]): PersonSheet[]{
+        var personSheet :PersonSheet[]
+        for(const item of elements){
+            personSheet.push(PersonSheet.fromSheetElement(item))
+        }
+        return personSheet
+    }
+
+    static fromSheetElement(element:any[]): PersonSheet{
+        return new PersonSheet(
+            element[0]==="*",
+            element[1]==="*",
+            element[2],
+            element[3],
+            element[4]
+        )
+    }
+
+    static fromPerson(person:Person){
+        return new PersonSheet(
+            false,
+            false,
+            person.personName,
+            person.slackId,
+            "",//new Date().toDateString()
+        )
+    }
+
 }
 
 
-function test_SheetHadler (){
+function testSheetHadler (){
     const sheetHandler: SheetHandler = new SheetHandler();
     Logger.log(sheetHandler.personsDataArray)
-    var person =sheetHandler.getWhoIsOnDuty();
-    Logger.log(person)
+    sheetHandler.writeNewPerson(new Person("テスト","xxxx"))
+}
+
+function testSheetHadlerChangeNextDuty(){
+    const sheetHandler = new SheetHandler();
+    sheetHandler.changeNextDuty()
 }
--- gas/src/main.ts
+++ gas/src/main.ts
@@ -1,14 +1,21 @@
-type MethodType = "getDuty" | "isRoot" | undefined;
+type MethodType = "getDuty" | "isRoot" | "writeName" | "changeNextDuty" |undefined;
 class QueryParam {
     methodType: MethodType;
-
+    name:string;
+    id:string;
     constructor(query:string){
         try{
             var methodString :string
-            query.split("&").forEach((s)=>{
-                if (s.split("=")[0]==="method")
+            query.split("&").forEach((param)=>{
+                const key = param.split("=")[0]
+                const value = param.split("=")[1]
+                if (key==="method")
                 {
-                    methodString=s.split("=")[1]
+                    methodString=value
+                }else if(key==="name"){
+                    this.name=value
+                }else if(key==="id"){
+                    this.id=value
                 }
             })
             
@@ -16,30 +23,59 @@ class QueryParam {
                 this.methodType = "getDuty";
             }else if (methodString === "isRoot"){
                 this.methodType = "isRoot";
+            }else if(methodString === "writeName"){
+                this.methodType = "writeName";
+            }else if(methodString === "changeNextDuty"){
+                this.methodType = "changeNextDuty"
             }else{
-                console.log(methodString)
+                console.warn("未定義なmethodが呼び出されました。");
+                console.warn(methodString);
             }
         }catch(error){
-            console.warn(error)
+            console.warn(error);
             this.methodType = undefined;
         }
     }
 }
 
 function doGet(e:GoogleAppsScript.Events.DoGet){
-    var method : QueryParam
+    var query : QueryParam
     
-    method = new QueryParam(e.queryString)
-    Logger.log("[GET Method]")
-    Logger.log(e.queryString)
-    if(method.methodType===undefined){
+    query = new QueryParam(e.queryString);
+    Logger.log("[GET Method]");
+    Logger.log(e.parameter);
+    if(query.methodType===undefined){
         return ContentService.createTextOutput("");
     }
-    else if(method.methodType==="getDuty"){
+    else if(query.methodType==="getDuty"){
         const onDutyPerson = new SheetHandler().getWhoIsOnDuty();
         return ContentService.createTextOutput(onDutyPerson.toJson());
-    }else if(method.methodType==="isRoot"){
-        return ContentService.createTextOutput("isRoot")
+    }
+    else if(query.methodType==="isRoot"){
+        const person = new Person("Hidden",query.id)
+        return ContentService.createTextOutput(String(new SheetHandler().checkRoot(person)));
+    }else{
+        return ContentService.createTextOutput("BadRequest.");
     }
 }
 
+function doPost(e:GoogleAppsScript.Events.DoPost){
+    var query = new QueryParam(e.queryString);
+    Logger.log("[POST Method]");
+    Logger.log(e.parameter);
+    
+    if(query.methodType===undefined){
+        return ContentService.createTextOutput("");
+    }
+    else if(query.methodType==="writeName"){
+        const sheetHandler = new SheetHandler();
+        sheetHandler.writeNewPerson(new Person(query.name,query.id));
+        return ContentService.createTextOutput("Request OK.");
+    }else if(query.methodType==="changeNextDuty"){
+        new SheetHandler().changeNextDuty()
+        return ContentService.createTextOutput("Request OK.");
+    }else{
+        return ContentService.createTextOutput("BadRequest.");
+    }
+
+}
